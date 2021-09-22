e4c01be91f3a4610730b2e6f94c27d3c1e8e0a6e
Gaurav Thakur <development@lmdmax.com>
2021-09-22 17:19:08 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,96 @@
+import React, {PureComponent} from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {RNCamera} from 'react-native-camera';
+import RNTextDetector from 'rn-text-detector';
+
+const PendingView = () => (
+  <View
+    style={{
+      flex: 1,
+      backgroundColor: 'lightgreen',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+    <Text>Waiting</Text>
+  </View>
+);
+
+class App extends PureComponent {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View >
+          <RNCamera
+            style={styles.preview}
+            type={RNCamera.Constants.Type.back}
+            flashMode={RNCamera.Constants.FlashMode.off}
+            androidCameraPermissionOptions={{
+                  title: 'Permission to use camera',
+                  message: 'We need your permission to use your camera',
+                  buttonPositive: 'Ok',
+                  buttonNegative: 'Cancel',
+            }}
+            androidRecordAudioPermissionOptions={{
+                  title: 'Permission to use audio recording',
+                  message: 'We need your permission to use your audio',
+                  buttonPositive: 'Ok',
+                  buttonNegative: 'Cancel',
+            }}>
+            {({camera, status, recordAudioPermissionStatus}) => {
+            if (status !== 'READY') return <PendingView />;
+            return (
+                  <View
+                  style={{
+                        flex: 1,
+                        flexDirection: 'row',
+                        justifyContent: 'center',
+                  }}>
+                  <TouchableOpacity
+                        onPress={() => this.takePicture(camera)}
+                        style={styles.capture}>
+                        <Text style={{fontSize: 14}}> SNAP </Text>
+                  </TouchableOpacity>
+                  </View>
+            );
+            }}
+            </RNCamera>
+      </View>
+</View>
+);}
+
+takePicture = async function (camera) {
+      const options = {quality: 0.8, base64: true};
+      const {uri} = await camera.takePictureAsync(options);
+      //  eslint-disable-next-line
+      console.log(uri);
+      const visionResp = await RNTextDetector.detectFromUri(uri);
+      console.log('visionResp', visionResp);
+      visionResp.map(data => {
+            console.log(data.text);
+      });
+      };
+}
+
+const styles = StyleSheet.create({
+      container: {
+      height: '100%',
+      flex: 1,
+      flexDirection: 'column',
+      backgroundColor: 'black',
+      },
+      preview: {
+      flex: 1,
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+      },
+      capture: {
+      flex: 0,
+      backgroundColor: '#fff',
+      borderRadius: 5,
+      padding: 15,
+      paddingHorizontal: 20,
+      alignSelf: 'center',
+      margin: 20,
+      },
+});
+export default App;
